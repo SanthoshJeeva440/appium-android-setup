f0522aa3401073fbdc4e96952b7bb745e82623d3
Santhosh <santhoshjeeva440@gmail.com>
2024-08-27 17:17:08 +0530

    fix github

--- runappium.sh
+++ runappium.sh
@@ -1,3 +1,4 @@
 npm install appium --save-dev --no-audit
 npx appium driver install uiautomator2
 npx appium --address 127.0.0.1 --port 4723 --base-path /wd/hub  --allow-cors
+python3 -m robot -d ./reports ./test/Test.robot
