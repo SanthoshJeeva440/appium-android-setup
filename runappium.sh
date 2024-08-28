906d3661175b03ee10e00370890a1c4a7421d0ad
Santhosh.G <100211577+SanthoshJeeva440@users.noreply.github.com>
2024-08-28 15:53:27 +0530

    Update runappium.sh
    Update fix

--- runappium.sh
+++ runappium.sh
@@ -1,5 +1,5 @@
 npm install appium --save-dev --no-audit
 npx appium driver install uiautomator2
 echo "Starting Appium ..."
-npx appium --address 127.0.0.1 --port 4723 --base-path /wd/hub  --allow-cors --command-timeout 150000 &>/dev/null &
+npx appium --address 127.0.0.1 --port 4723 --base-path /wd/hub  --allow-cors --command-timeout 150000&>/dev/null &
 Sleep 10
